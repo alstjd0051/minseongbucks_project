825b22cb386b361ca61e0ebd95107c9af244ef33
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-05-26 02:43:18 +0900

    드롭다운메뉴 끝

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function() {
+searchEl.addEventListener('click', function () {
   //Logic..
   searchInputEl.focus();
 });
@@ -15,3 +15,27 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+// 뱃지 
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // badgeEl.style.display = 'none';
+    // gsap.to(요소, 지속시간 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity : 0,
+      display : 'none'
+    });
+  } else {
+    // 배지 보이기
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity : 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
