7ab9f0b624d0e876ff01c44d415ee6c4a75368b9
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-05-28 02:59:53 +0900

    슬라이드 영역 토글

--- js/main.js
+++ js/main.js
@@ -61,20 +61,34 @@ new Swiper('.notice-line .swiper-container', {
 });
 
 new Swiper('.promotion .swiper-container', {
-  slidesPerView:3, //한번에 보여줄 슬라이드 개수
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
   spaceBetween: 10, //슬라이드 사이 여백
   centeredSlider: true, //1번 슬라이드가 가운데 보이기
   loop: true,
   // autoplay: { // 자동 재생 여부
   //   delay: 5000 // 5초마다 슬라이드 바뀜
   // },
-  pagination : {
-    el:'.promotion .swiper-pagination', //페이지 번호 요선택자
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요선택자
     clickable: true //사용자의 페이지 번호 요소 제어 가능 여부
 
   },
-  navigation : {
+  navigation: {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
   }
 })
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    //true = 숨김처리
+    promotionEl.classList.add('hide');
+  } else {
+    //false = 보임처리
+    promotionEl.classList.remove('hide');
+  }
+});
