7b6cf5dce2922a4a94cf051e35c1e8e4c1b81a3d
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-05-28 02:46:20 +0900

    프로모션 이미지 슬라이드

--- js/main.js
+++ js/main.js
@@ -41,17 +41,40 @@ window.addEventListener('scroll', _.throttle(function () {
 // _.throttle(함수, 시간)
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function(fadeEl, index) {
+fadeEls.forEach(function (fadeEl, index) {
   // gsap.to(요소, 지속시간 {옵션});
   gsap.to(fadeEl, 1, {
-    delay: (index +1) * .7, //first fade-in ->0.7s second 1.4....
+    delay: (index + 1) * .7, //first fade-in ->0.7s second 1.4....
     opacity: 1
   });
 });
 
+
+/**
+ * 슬라이드 요소 관리
+ */
 // new Swiper(선택자,옵션)
 new Swiper('.notice-line .swiper-container', {
-  direction: 'vertical',
-  autoplay:true,
-  loop: true
+  direction: 'vertical', // 수직 슬라이드
+  // autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
 });
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView:3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlider: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: { // 자동 재생 여부
+  //   delay: 5000 // 5초마다 슬라이드 바뀜
+  // },
+  pagination : {
+    el:'.promotion .swiper-pagination', //페이지 번호 요선택자
+    clickable: true //사용자의 페이지 번호 요소 제어 가능 여부
+
+  },
+  navigation : {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+})
