1cc7f415063d74693ac898db0576475e3cad5af0
alstjd0051 <wsc03002@naver.com>
2021-07-23 01:18:40 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
 'use strict'
 
-/**
- * 검색창 제어
- */
-// 검색창 요소(.search) 찾기.
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-// 검색창 요소를 클릭하면 실행.
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus()
-})
-// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused')
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
-})
+
 
 
 /**
@@ -43,7 +24,7 @@ window.addEventListener('scroll', _.throttle(function () {
       x: 0
     })
 
-  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    // 페이지 스크롤 위치가 500px이 넘지 않으면.
   } else {
     // Badge 요소 보이기!
     gsap.to(badgeEl, .6, {
@@ -136,7 +117,7 @@ promotionToggleBtn.addEventListener('click', function () {
   // 요소를 숨겨야 하면,
   if (isHidePromotion) {
     promotionEl.classList.add('hide')
-  // 요소가 보여야 하면,
+    // 요소가 보여야 하면,
   } else {
     promotionEl.classList.remove('hide')
   }
@@ -188,8 +169,3 @@ spyEls.forEach(function (spyEl) {
 })
 
 
-/**
- * 올해가 몇 년도인지 계산
- */
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
